3f258f5df553e87f43c1a184ece095cd5974a4db
xansar1 <xansar89@gmail.com>
2026-04-09 11:05:42 +0530

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -1,1 +1,10 @@
+import hashlib
 
+USERS = {
+    "admin": hashlib.sha256("admin123".encode()).hexdigest(),
+    "college": hashlib.sha256("college123".encode()).hexdigest()
+}
+
+def check_login(username, password):
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    return USERS.get(username) == hashed
