4789118587ec25e9ea8ad210ab85791770c524fb
xansar1 <xansar89@gmail.com>
2026-04-09 19:53:40 +0530

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://postgres:password@db:5432/academic_ai"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
