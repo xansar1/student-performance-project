005b8a3df7c732e749df523b5122c0177b7b8883
Muhammed Ansar N <xansar89@gmail.com>
2026-04-11 17:05:50 +0530

    Update parent_portal.py

--- core/parent_portal.py
+++ core/parent_portal.py
@@ -1,9 +1,9 @@
 def get_parent_student_record(df, parent_username):
-    student_name = parent_username.replace("_parent", "")
-    student_name = student_name.strip().lower()
+    admission_no = parent_username.replace("P_", "").upper()
 
     row = df[
-        df["STUDENT_NAME"].str.lower() == student_name
+        df["ADMISSION_NO"].astype(str).str.upper()
+        == admission_no
     ]
 
     if row.empty:
