89c443dff9daaf39d1d005b1e080ec1d3a284723
Muhammed Ansar N <xansar89@gmail.com>
2026-04-11 16:16:57 +0530

    Update student_auth.py

--- core/student_auth.py
+++ core/student_auth.py
@@ -2,10 +2,14 @@ import hashlib
 
 STUDENT_USERS = {
     "aarav": hashlib.sha256("student123".encode()).hexdigest(),
-    "meera": hashlib.sha256("student123".encode()).hexdigest()
+    "meera": hashlib.sha256("student123".encode()).hexdigest(),
 }
 
 
 def student_login(username, password):
+    username = username.strip().lower()
+    password = password.strip()
+
     hashed = hashlib.sha256(password.encode()).hexdigest()
+
     return STUDENT_USERS.get(username) == hashed
