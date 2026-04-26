f047a69b3a2d9a2c9e4db3d45bdca1377f321f18
Muhammed Ansar N <xansar89@gmail.com>
2026-04-26 13:56:07 +0530

    Delete frontent directory

--- frontent/script.js
+++
@@ -1,9 +0,0 @@
-async function loadData() {
-  const res = await fetch("https://ai-academic-backend.onrender.com/analytics/kpis");
-  const data = await res.json();
-
-  document.getElementById("students").innerText = data.total_students;
-  document.getElementById("avg").innerText = data.avg_score;
-  document.getElementById("top").innerText = data.top_score;
-  document.getElementById("risk").innerText = data.at_risk;
-}
